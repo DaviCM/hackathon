63a47eeda156d6c45903cab9461bf40f36ee04e5
nunes <arthurnunescarvalho2@gmail.com>
2025-10-15 07:30:15 -0300

    main update

--- main_api.py
+++ main_api.py
@@ -1,10 +1,11 @@
 #importa o fastapi
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 #uvicorn main_api:app --reload
 # para rodar no localhost
 
-#python -m uvicorn main_api:app --host 0.0.0.0 --port 80
+#uvicorn main_api:app --host 0.0.0.0 --port 80
 # para roda no shardecloud
 
 #pega os script feito pelo Coelho
@@ -17,23 +18,32 @@ import src.maregrafos as maregrafos
 #cria um obj do fastapi
 app = FastAPI()
 
-@app.get("/meteorologia/{tempo}")
+#Middleware CORS -> facilita arequisição no js
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/meterologia/{tempo}")
 def meterologia(tempo: float):
-    return [*meteorology(tempo)]
+    return {"result": [*meteorology(tempo)]}
 
 @app.get("/noticias/{dias_atras}")
 def noticias(dias_atras: int):
-    return [*parse_news(dias_atras)]
+    return {"result": [*parse_news(dias_atras)]}
 
 @app.get("/nivel_do_mar/{tempo}")
 def nivel_do_mar(tempo: float):
-    return [*sea_level(tempo)]
+    return {"result": [*sea_level(tempo)]}
 
 @app.get("/maregrafo_info")
 def get_maregrafo_info():
-    return [*maregrafos.get_maregrafo_info()]
+    return {"result": [*maregrafos.get_maregrafo_info()]}
 
 @app.get("/")
 def home():
-    return "vá para /docs ou morra"
+    return "vá para https://mocate2ds.shardweb.app/docs ou morra"
 
